4a607bde37e82cb42c7bf5d1d904eae82b1409f4
pierrbt <pierre08bt@gmx.co.uk>
2023-07-18 21:49:44 +0200

    fix: add verify token function

--- src/secrets.ts
+++ src/secrets.ts
@@ -10,3 +10,13 @@ export function createToken(id: number): string {
     { expiresIn: "5y", issuer: "Griff's API", subject: "auth" },
   );
 }
+
+export function verifyToken(token: string): Promise<number> {
+  return new Promise((resolve, reject) => {
+    jwt.verify(token, jwtSecret, (err: any, decoded: any) => {
+      if (err) return reject(err);
+      if (decoded.id) resolve(decoded.id);
+      else reject(0);
+    });
+  });
+}
