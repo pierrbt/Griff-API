1a94e40dcb0b43da9c80593fa71443067692ec3e
pierrbt <pierre08bt@gmx.co.uk>
2023-10-21 20:37:44 +0200

    fix: add zod and try/catch in auth routes

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -2,83 +2,87 @@ import { Express } from "express";
 import bcrypt from "bcrypt";
 import { createToken } from "../tokens";
 import { prisma, authToken } from "../middleware";
+import { z } from "zod";
+
+const loginUserObject = z.object({
+  pseudo: z.string().min(3, "Pseudo must be at least 3 characters long"),
+  password: z.string().min(8, "Password must be at least 8 characters long"),
+});
 
 export default function declareAuthRoutes(app: Express) {
   app.post("/login", async (req, res) => {
-    const { pseudo, password } = req.body;
+    try {
+      const parsed = loginUserObject.safeParse(req.body);
+      if (!parsed.success) {
+        throw {
+          status: 400,
+          message: "Invalid parameters " + parsed.error.message,
+        };
+      }
 
-    if (!pseudo || !password)
-      return res.status(400).send({
-        ok: false,
-        message: "Missing parameters : (email, password) required",
-      });
+      const { pseudo, password } = parsed.data;
 
-    await prisma.user
-      .findUnique({
+      const user = await prisma.user.findUnique({
         where: {
           pseudo,
         },
-      })
-      .then(async (user: any) => {
-        if (!user)
-          return res.status(404).send({
-            ok: false,
-            message: "User not found",
-          });
-
-        const valid = await bcrypt.compare(password, user.password);
-        if (!valid)
-          return res.status(401).send({
-            ok: false,
-            message: "Invalid password",
-          });
-
-        const token = createToken(user.id);
-        console.log(token);
-        res.status(200).send({
-          ok: true,
-          message: "User logged in",
-          user: user,
-          token: token,
-        });
-      })
-      .catch((err: any) => {
-        res.status(500).send({
-          ok: false,
-          message: "Error while logging in",
-          error: err,
-        });
       });
+
+      if (!user || !(await bcrypt.compare(password, user.password))) {
+        throw {
+          status: 401,
+          message: "Incorrect username or password",
+        };
+      }
+      const token = createToken(user.id);
+      res.status(200).send({
+        ok: true,
+        message: "User logged in",
+        user: user,
+        token: token,
+      });
+    } catch (error: any) {
+      const status = error.status || 500;
+      const message = error.message || "Error while getting user";
+      res.status(status).send({
+        ok: false,
+        message: message,
+      });
+    }
   });
 
   app.post("/user/verify", authToken, async (req, res) => {
-    const userId = res.locals.userId;
-    return await prisma.user
-      .findUnique({
+    try {
+      const userId = res.locals.userId;
+      const user = await prisma.user.findUnique({
         where: {
           id: userId,
           status: "active",
         },
-      })
-      .then((user: any) => {
-        if (!user)
-          return res.status(404).send({
-            ok: false,
-            message: "User not found",
-          });
-
-        return res.status(200).send({
-          ok: true,
-          message: "User verified",
-          user: user,
-        });
-      })
-      .catch((err: any) => {
-        return res.status(500).send({
-          ok: false,
-          message: "Error while verifying user",
-          error: err,
-        });
+        select: {
+          password: false,
+        },
       });
+
+      if (!user) {
+        throw {
+          status: 404,
+          message: "User not found",
+        };
+      }
+
+      res.status(200).send({
+        ok: true,
+        message: "User verified",
+        user: user,
+      });
+    } catch (error: any) {
+      const status = error.status || 500;
+      const message = error.message || "Error while getting user";
+      res.status(status).send({
+        ok: false,
+        message: message,
+      });
+    }
   });
 }
