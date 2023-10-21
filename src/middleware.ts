0e57afe7c7bfaac9cc84eeb025280aa92240d6be
pierrbt <pierre08bt@gmx.co.uk>
2023-10-21 20:14:33 +0200

    feat: implement new middlewares

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,6 +4,26 @@ import { checkAndVerifyToken } from "./tokens";
 
 export const prisma = new PrismaClient();
 
+export async function updateLastActivity(
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) {
+  const token = req.headers.authorization;
+  const [authId, message] = await checkAndVerifyToken(token);
+  if (authId) {
+    prisma.user.update({
+      where: {
+        id: authId,
+      },
+      data: {
+        lastActivity: new Date(),
+      },
+    });
+  }
+  next();
+}
+
 export async function authToken(
   req: Request,
   res: Response,
@@ -18,14 +38,37 @@ export async function authToken(
     });
   }
 
-  prisma.user.update({
+  res.locals.userId = authId;
+  next();
+}
+
+export async function authAdmin(
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) {
+  const token = req.headers.authorization;
+  const [authId, message] = await checkAndVerifyToken(token);
+  if (!authId) {
+    return res.status(401).send({
+      ok: false,
+      message: `Auth failed, ${message}`,
+    });
+  }
+
+  const user = await prisma.user.findUnique({
     where: {
       id: authId,
+      admin: true,
     },
-    data: {
-      lastActivity: new Date(),
-    }
-  })
+  });
+
+  if (!user) {
+    return res.status(401).send({
+      ok: false,
+      message: `User not found or not admin`,
+    });
+  }
 
   res.locals.userId = authId;
   next();
