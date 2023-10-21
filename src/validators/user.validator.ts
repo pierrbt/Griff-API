4907492a6603acdf6d935668eb8cb5942eca3112
pierrbt <pierre08bt@gmx.co.uk>
2023-10-21 23:12:44 +0200

    refactor: change export statement

--- src/validators/user.validator.ts
+++ src/validators/user.validator.ts
@@ -1,18 +1,18 @@
 import { z } from "zod";
 
-export const userIdParameter = z.coerce
+const userIdParameter = z.coerce
   .number()
   .positive("User id must be positive")
   .int();
 
-export const createUserObject = z.object({
+const createUserObject = z.object({
   pseudo: z.string().min(3, "Pseudo must be at least 3 characters long"),
   firstName: z.string().min(3, "First name must be at least 3 characters long"),
   email: z.string().email("Invalid email"),
   password: z.string().min(8, "Password must be at least 8 characters long"),
 });
 
-export const updateUserObject = z
+const updateUserObject = z
   .object({
     pseudo: z.string().min(3, "Pseudo must be at least 3 characters long"),
     firstName: z
@@ -24,3 +24,5 @@ export const updateUserObject = z
   .refine((data) => Object.keys(data).length > 0, {
     message: "At least one parameter is required",
   });
+
+export { userIdParameter, createUserObject, updateUserObject };
