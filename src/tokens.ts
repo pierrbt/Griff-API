d8997b75a45a64b87b0c9674dd7fd2d1e07aec0f
pierrbt <pierre08bt@gmx.co.uk>
2023-10-21 20:15:08 +0200

    refactor: format, fix: simplify jwt decoding

--- src/tokens.ts
+++ src/tokens.ts
@@ -1,9 +1,9 @@
 import jwt from "jsonwebtoken";
 import dotenv from "dotenv";
-import { confirm } from '@inquirer/prompts';
+import { confirm } from "@inquirer/prompts";
 import crypto from "crypto";
-import {writeFile} from "fs/promises";
-import {prisma} from "./middleware";
+import { writeFile } from "fs/promises";
+import { prisma } from "./middleware";
 
 dotenv.config();
 
@@ -12,24 +12,23 @@ let jwtSecret = process.env.TOKEN_SECRET as string;
 if (!process.env.TOKEN_SECRET) {
   console.error("[ERROR]: No token secret provided (.env file, TOKEN_SECRET)");
   confirm({
-    message: 'Do you want to generate a new token secret or want to do it yourself?',
-  }).then(async (answer: boolean) => {
-    if(!answer)
-      process.exit(1);
-    const secret = crypto.randomBytes(128).toString('hex');
-    console.log(`Generated secret: ${secret.slice(0, 20)}...`);
-    await writeFile('.env', `TOKEN_SECRET=${secret}`);
-    console.log('Secret saved to .env file');
-    process.env.TOKEN_SECRET = secret;
-    jwtSecret = secret;
+    message:
+      "Do you want to generate a new token secret or want to do it yourself?",
   })
+    .then(async (answer: boolean) => {
+      if (!answer) process.exit(1);
+      const secret = crypto.randomBytes(128).toString("hex");
+      console.log(`Generated secret: ${secret.slice(0, 20)}...`);
+      await writeFile(".env", `TOKEN_SECRET=${secret}`);
+      console.log("Secret saved to .env file");
+      process.env.TOKEN_SECRET = secret;
+      jwtSecret = secret;
+    })
     .catch((error: any) => {
       console.error(error);
       process.exit(1);
-  });
+    });
 }
-
-
 
 export function createToken(id: number): string {
   return jwt.sign(
@@ -53,21 +52,18 @@ export async function checkAndVerifyToken(
     const decoded: any = jwt.verify(cleanedToken, jwtSecret);
 
     if (decoded && decoded.id && typeof decoded.id === "number") {
-      return await prisma.user.findUnique({
+      const user = await prisma.user.findUnique({
         where: {
           id: decoded.id,
           status: "active",
-        }
-      })
-        .then((user: any) => {
-          if (!user)
-            throw new Error("User has been deleted or banned");
-          return [decoded.id, "User is valid"];
-        })
-        .catch((error: any) => {
-          console.log(`[ERROR] error while verifying token: ${error.message}`);
-          return [0, error.message];
-        }) as [number, string];
+        },
+      });
+
+      if (!user) {
+        throw new Error("User has been deleted or banned");
+      }
+
+      return [decoded.id, "User is valid"];
     } else {
       throw new Error("Invalid token content");
     }
